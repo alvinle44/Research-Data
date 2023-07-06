bcd54f251e3526a7454b1ae50c6eb1a655c67422
Alvin Le <alvinle44@gmail.com>
2023-07-05 21:03:37 -0700

    created function that outputs graph of desried data type

--- Research_Data_Processing.py
+++ Research_Data_Processing.py
@@ -10,7 +10,9 @@ class Treatment:
     DEFAULT_MAX_LETTERS = 30
     DEFAULT_NAME = "Unnamed"
     DEFAULT_TREATMENTS = []
+    DEFAULT_AGE_GROUPS = []
     DEFAULT_DATA_INDICATION = ["RFP Nucleus", "GFP Nucleus", "RFP Cytoplasm"]
+    DATA_COLUMNS = ['Area', 'Mean', 'IntDen', 'RawIntDen']
 
 
     number_of_treatments = 0
@@ -18,12 +20,13 @@ class Treatment:
         self.dataset = self.DEFAULT_DATASET
         self.name = self.DEFAULT_NAME
         self.treatment = self.DEFAULT_TREATMENTS
+        self.ages = self.DEFAULT_AGE_GROUPS
     
 
     @property
     def name(self):
         #getter for datasetname
-        return self.name
+        return self._name
     
 
     @name.setter
@@ -37,7 +40,7 @@ class Treatment:
     def process_file(self, filename):
         """
         This method is able to read in the csv file as a pandas dataframe.
-        This process is able to extract all the treatments that were conducted in the experiment.
+        This process is able to extract all the treatments and ages that were conducted in the experiment.
         """
         try:
             df = pd.read_csv(filename).dropna()
@@ -47,6 +50,9 @@ class Treatment:
         treatments = self.dataset['Treatment'].unique()
         for treatment in treatments:
             self.treatment.append(treatment)
+        age_groups = self.dataset['Age Group'].unique()
+        for age in age_groups:
+            self.ages.append(age)
         return True
 
     def clean_data(self, treatment_name):
@@ -65,12 +71,63 @@ class Treatment:
                     break
 
 
+    def get_averages(self, column_name, grouping):
+        if column_name not in Treatment.DATA_COLUMNS:
+            return False
+        # sort treatments and age into either x or y axis 
+        if grouping ==  1:
+            x_axis, y_axis = tuple(self.treatment), tuple(self.ages)
+            slot_1, slot_2  = "Treatment", "Age Group"
+        else:
+            x_axis, y_axis = tuple(self.ages), tuple(self.treatment)
+            slot_1, slot_2 = "Age Group", "Treatment"
+        #columns are by age group 
+        #this dictioanry will be used to be plot later 
+        averages = {}
+        st_devs = {}
+        for y in y_axis:
+            averages[y] = []
+            st_devs[y] = []
+        for x_item in x_axis:
+            #this list is appended to the averages dictionary along with its y_item as its key
+            for y_item in y_axis:
+                average = self.dataset.loc[((self.dataset[slot_1] == x_item) & (self.dataset[slot_2] == y_item)), column_name].mean()
+                averages[y_item].append(average)
+                st_dev = self.dataset.loc[((self.dataset[slot_1] == x_item) & (self.dataset[slot_2] == y_item)), column_name].std()
+                st_devs[y_item].append(st_dev)
+        graph = np.arange(len(x_axis))
+        width = 0.15
+        multiplier = 0
+
+        fig, ax = plt.subplots(layout='constrained')
+        for attribute, measurement in averages.items():
+            offset = width * multiplier
+            rects = ax.bar(graph + offset, measurement, width, label=attribute)
+            ax.bar_label(rects, padding=3)
+            multiplier += 1
+        ax.set_ylabel(column_name)
+        ax.set_title(self.name)
+        ax.set_xticks(graph + width, x_axis)
+        ax.legend(loc='upper left', ncols=3)
+        ax.set_ylim(0, 300)
+
+        plt.show()
+
+
+    def print_graphs(self, ave , stdev):
+        pass
 
     def print_df(self):
         print(self.dataset)            
 
 def abstract_data(filename):
-    name_input_data = input("Please input a valid CSV file name. ")
+    go = True
+    while go:
+        try:
+            name_input_data = input("Please input a valid CSV file name. ")
+            go = False
+        except FileNotFoundError:
+            print("Give Valid File!")
     if filename.process_file(name_input_data):
         while True:
             try:
@@ -83,17 +140,36 @@ def abstract_data(filename):
     else:
         print("Unsuccessful attempt to load data.")
         return False
+    
 
+def present_graph(datafile):
+    """
+    Present all of the cleaned data in a graph with treatments grouped up and age as well.
+    Can also present just the values of the averages of each treatment group and age group. 
+    """
+    while True:
+        try:
+            graph_rep = int(input("Indicate what grouping you would like to visualize by either typing" 
+                            " 1 for grouping by age group or 2 for grouping by treatment group. "))
+        except ValueError:
+            print("Type a valid number as instructed for data visualization.")
+        column_data = input("Type Area, Mean, or IntDen for data display. ")
+        datafile.get_averages(column_data, graph_rep)
+        
+    
+        
+    
 
 def print_menu():
     print("\nMain Menu\n"
           "1 - Process a new data file\n"
-          "2 - Print Data Chart/Image\n"
-          "3 - Retrieve Data\n"
+          "2 - Print Treatment and Ages\n"
+          "3 - Print Chart\n"
           "4 - Quit\n")
     
-def print_treatments(datafile):
+def print_treatments_ages(datafile):
     print(datafile.treatment)
+    print(datafile.ages)
 
 
 def main():
@@ -110,16 +186,10 @@ def main():
         if menu_choice == 1:
             abstract_data(new_set)
         elif menu_choice == 2:
-            print_treatments(new_set)
+            print_treatments_ages(new_set)
         elif menu_choice == 3:
-            pass
+            present_graph(new_set)
         elif menu_choice == 4:
-            pass
-        elif menu_choice == 5:
-            pass
-        elif menu_choice == 6:
-            pass
-        elif menu_choice == 7:
             break
         else:
             print("Invalid Choice, please enter an integer between 1 and 7.\n")
@@ -128,10 +198,3 @@ def main():
 if __name__ == "__main__":
     """Call on function main."""
     main()
-
-
-
-
-
-
-
---
+++ test.py
@@ -0,0 +1,6 @@
+dics = {}
+litt = [1,2,3,4]
+for i in litt:
+    dics[i] = []
+dics[1].append(3)
+print(dics)
